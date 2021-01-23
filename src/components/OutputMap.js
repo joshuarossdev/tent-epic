12820995629e889dd95e911c37239b6fd14e41dc
Joshua Ross <jjross.dev@gmail.com>
2021-01-22 21:52:48 -0800

    Delete create map component. Add google-map-react module to OutputMap

---
+++ src/components/OutputMap.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import '../css/OutputMap.css'
+import GoogleMapReact from 'google-map-react'
+export default function OutputMap () {
+
+  return (
+    <div>
+      Output Map
+    </div>
+  )
+}
